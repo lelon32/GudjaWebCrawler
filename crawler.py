707d88a8393bc083fc049eb28e943f8a2ba26b1d
freeman-bw <freembre@oregonstate.edu>
2019-01-27 16:26:53 -0800

    Made some minor modifications so that the code accepts a url as an argument. I amde a throwaway function to output the get request into a text file so i practice on a static file rather than bombardinga live site with my requests (and avoid the risk of getting blacklisted)

--- crawler.py
+++ crawler.py
@@ -6,16 +6,52 @@ from bs4 import BeautifulSoup
 
 class crawler():
 
-    def __init__(self):
+    def __init__(self, url):
+        self.url = url
         self.web_links = []
         self.favicon = ''
 
 
-    def get_all_links(self, url):
-        r = requests.get(url)
+    def get_all_links(self):
+        r = requests.get(self.url)
         soup = BeautifulSoup(r.text, 'html.parser')
         count = 0
         for link in soup.find_all('a'):
             self.web_links.append(link.get('href'))
 
             #print(link.get('href'))
+
+
+    def get_rel_links(self):
+        r = requests.get(self.url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        count = 0
+        for link in soup.find_all('a'):
+            self.web_links.append(link.get('href'))
+
+    def get_favicon(self):
+        # need to add some checks here to return a blank or default favicon if none exists
+        self.favicon = self.url + "/favicon.ico"
+
+    # method to check that urls have correct syntax
+    def check_urls(self):
+        pass
+
+    # method to add links to data structure
+    def build_tree(self):
+        pass
+
+
+    # method to return data to server
+    def send_tree(self):
+        pass
+
+
+
+    # I created this so we can hava static document to test on
+    def write_to_file(self):
+        r = requests.get(self.url)
+        f = open("giz.txt", "w+")
+        for line in r.text:
+            f.write(line)
+        f.close()
--- dfs.py
+++ dfs.py
@@ -1,4 +1,5 @@
 from crawler import crawler
+import sys
 
 class dfs():
     pass
@@ -6,9 +7,27 @@ class dfs():
 
 
 
-dfs_crawl = crawler()
 
-dfs_crawl.get_all_links("https://www.cnn.com")
+# Code Execution begins here, may move to another file
+dfs_crawl = crawler(sys.argv[1])
 
-for link in dfs_crawl.web_links:
-    print(link)
+#dfs_crawl.get_all_links()
+dfs_crawl.write_to_file()
+#for link in dfs_crawl.web_links:
+#    print(link)
+
+
+# Resources I used
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+# http://blog.adnansiddiqi.me/tag/scraping/
+# https://www.sohamkamani.com/blog/2015/08/21/python-nodejs-comm/
+# https://stackoverflow.com/questions/23450534/how-to-call-a-python-function-from-node-js
+# https://stackoverflow.com/questions/48136501/run-python-script-from-index-js-cloud-function
+# https://stackoverflow.com/questions/45092342/using-google-cloud-function-to-spawn-a-python-script
+# https://www.scrapehero.com/how-to-prevent-getting-blacklisted-while-scraping/
+# https://stackoverflow.com/questions/43052290/representing-a-graph-in-json
+# https://www.reddit.com/r/learnpython/comments/1br6u6/how_do_i_pass_arguments_to_a_py_script_in_pycharm/
+#
+#
+# https://www.jetbrains.com/help/pycharm/commit-and-push-changes.html#push
+#
