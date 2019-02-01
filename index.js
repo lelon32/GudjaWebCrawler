147ae85698b383205c608b4c46e84ddb5dfcdf6a
Kerensa Crump <kerensacrump@Kerensas-MacBook-Pro-3.local>
2019-01-31 20:31:52 -0500

    index.js sends static angular files

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
 
-app.get('/', function (req, res) {
-	   res.send('Hello World from GCE!');
+const app = express();
+
+app.use("/", express.static(path.join(__dirname, "front_end")));
+app.use((req, res, next) => {
+	res.sendFile(path.join(__dirname, 'front_end', 'index.html'));
 });
+
+module.exports = app;
 
 app.listen(3000, function () {
 	   console.log('Example app listening on port 3000!');
