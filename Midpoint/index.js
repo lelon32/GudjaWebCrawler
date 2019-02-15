2c68fed87ac161aab26c049ce3a320d7fb7d2b1a
Brent Freeman <freembre@oregonstate.edu>
2019-02-14 19:02:23 -0800

    this works on gcp

--- Midpoint/index.js
+++ Midpoint/index.js
@@ -23,29 +23,42 @@ function crawl_web(url) {
 
     //this piece give instruction on what to do after it is complete
     // instead of console .log this should return the value
-    py.stdout.on('end', function(){console.log('result=',dataString);});
+    py.stdout.on('end', function(){
+        
+        //make this do something else
+        console.log('result=',dataString)
+    
+    
+    ;});
 
     //here is where the data is written to std in to actually call the python function
     py.stdin.write(data);
     py.stdin.end();
 }
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/front_end/index.html'));
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
-router.post('/',function(req, res){
-    
+
+
+app.post('/',function(req, res){
+    console.log("beginning web crawl")
     crawl_web(req.body.url)
 
-     res.sendFile(path.join(__dirname+'/front_end/index.html'));
+     res.sendFile(path.join(__dirname+'/index.html'));
 
 });
 
 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
+// [END app]
+
+module.exports = app;
 
 console.log('Running at Port 3000');
